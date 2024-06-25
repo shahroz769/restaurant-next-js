67b497edaad6c2fa577a12caca8d54d2639946de
Shahroz Ahmed <shahroz.khan2124238@gmail.com>
2024-06-25 23:39:15 +0500

    first commit

---
+++ app/page.js
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+    return (
+        <main className={styles.main}>
+            <div className={styles.center}>
+                <div
+                    style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                        gap: "24px"
+                    }}
+                >
+                    <h1>Work In Progress</h1>
+                    <h1>BAIT AL AYAWED</h1>
+                </div>
+            </div>
+            <div className={styles.center}>
+                <Image
+                    className={styles.logo}
+                    src="/next.svg"
+                    alt="Next.js Logo"
+                    width={180}
+                    height={37}
+                    priority
+                />
+            </div>
+        </main>
+    );
+}
