47f5b58d53b8e84b488c9bc905384aeb339d9cbf
Shahroz Ahmed <shahroz.khan2124238@gmail.com>
2024-09-09 18:12:23 +0500

    Minor fix

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
-import Logo from '@/assets/images/logo1.png';
+import Logo from '@/assets/images/Logo1.png';
 
 export default function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
